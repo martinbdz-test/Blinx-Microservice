b793456b7ff4b8aa92afe2cb8d34ccccc0dab333
Bashorun Olajide John <bashorunolajide@gmail.com>
2022-10-12 23:06:44 +0100

    Added configuration for api gateway

--- user-identity-service/src/main/java/com/blinx/userservice/UserServiceApplication.java
+++ user-identity-service/src/main/java/com/blinx/userservice/UserServiceApplication.java
@@ -6,6 +6,7 @@ import com.blinx.userservice.service.UserService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,6 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import java.util.ArrayList;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
